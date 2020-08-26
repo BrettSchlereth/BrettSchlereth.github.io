248ca7a57f12805b4a1d050fcbf7da1e105df260
Brett Schlereth <brschle@gmail.com>
2020-08-26 14:47:12 -0500

    Updates

---
+++ precache-manifest.9bb8aa273a8af79083e2e84aca582741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1dfde0acb9c6f9f396e50c8b31ed7ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d52cdf99fdd638521683",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8230cd96b44f3e3fa53a",
+    "url": "/static/js/2.04adb1bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04adb1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52cdf99fdd638521683",
+    "url": "/static/js/main.b32374c2.chunk.js"
+  },
+  {
+    "revision": "93ccebd0741681011358",
+    "url": "/static/js/runtime-main.bbbe84dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
