278d5646f8710bf98b6823610bc18b64da7e0e35
Brett Schlereth <brschle@gmail.com>
2021-08-23 22:28:00 -0500

    Added more game logic. Still needs ai side.

--- src/components/BlackjackGame.js
+++ src/components/BlackjackGame.js
@@ -32,6 +32,47 @@ const cardValues = {
   11: aceOfSpades
 }
 
+class Hand {
+  constructor(handOwner) {
+    this.owner = handOwner;
+    this.cards = [];
+    this.soft = 0;
+    this.blackjack = false;
+    this.count = 0;
+    this.total = 0;
+    this.busted = false;
+  }
+
+  addCard(deck) {
+    this.cards.push(deck.pop())
+    this.getTotalCardValue()
+  }
+
+  getTotalCardValue() {
+    var total = 0
+    this.cards.forEach(card => {
+      if (card == 11 && total + card > 21) {
+        card = 1
+      }
+      else if (card == 11 && this.soft == 0) {
+        this.soft = 1
+      }
+      else if (total + card > 21 && this.soft == 1) {
+        total -= 10
+        this.soft = 0
+      }
+      total += card
+    });
+
+    this.total = total
+    if (total > 21) {
+      this.busted = true;
+    }
+    else {
+      this.busted = false;
+    }
+  }
+}
 
 const gameStyle = {
   background: 'brown',
@@ -76,74 +117,213 @@ const gameButton = {
 
 var deck;
 var aiDeck;
-var dealerHand;
-var playerHand;
-var aiDealerHand;
-var aiPlayerHand;
+var dealerHand = new Hand("dealer");
+var playerHand = new Hand("player");
+var aiDealerHand = new Hand("dealer");
+var aiPlayerHand = new Hand("player");
 
 class BlackjackGame extends React.Component {
   constructor(props) {
-    startGame()
     super(props);
+    this.startGame();
     this.state = {
       playerHand : [],
-      aiHand: []
+      dealerHand: [],
+      aiHand: [],
+      hitDisabled: false,
+      stayDisabled: false,
+      restartDisabled: true,
+      playerDone: false,
+      dealerDone: false,
+      message: "hit or stay",
+      aiMessage: "",
     }
   }
 
-  hit() {
-   console.log("hit")
-   console.log('playerHand', playerHand, 'aiHand', aiPlayerHand)
-   this.setState({
-     playerHand: playerHand.push(deck.pop())
-   },
-    () => {console.log(deck.length, aiDeck.length)})
+  checkForBlackjack(dealer, player) {
+    if (dealer.total == 21 && player.total == 21) {
+      this.setState({
+        message: "Push!",
+      });
+      return true;
+    }
+    else if (dealer.total == 21) {
+      this.setState({
+        message: "Dealer has blackjack!"
+      })
+      return true
+    }
+    else if (player.total == 21) {
+      this.setState({
+        message: "Player has blackjack!"
+      })
+      return true
+    }
+    else return false
+    }
+
+  getCards(hand, playerType) {
+    var cards = []
+    for (var i=0; i<hand.cards.length; i++) {
+        var card = hand.cards[i]
+        cards.push(getCard(card, i))
+        if (playerType === "dealer" & this.state.playerDone == false) {
+          i = hand.cards.length
+        }
+    }
+    return cards;
+  }
+
+  getDealerMoves() {
+    console.log("starting dealer moves")
+    while (dealerHand.total < 17) {
+      this.hit("dealer")
+    }
+    this.getWinner(dealerHand, playerHand)
+    this.setState({
+      restartDisabled: false
+    })
+  }
+
+  getWinner(dealer, player) {
+    var winnerMessage = ""
+    console.log("dealerTotal:", dealer.total)
+    if (dealer.busted) {
+      winnerMessage = "Dealer Busted! Player Wins!"
+    }
+    else if (dealer.total > player.total) {
+      winnerMessage = "Dealer Wins!"
+    }
+    else if (player.total > dealer.total) {
+      winnerMessage = "Player Wins!"
+    }
+    else {winnerMessage = "Push!"}
+    this.setState({
+      message: winnerMessage
+    })
+  }
+
+  hit(handOwner) {
+   switch (handOwner) {
+     case "player":
+       this.setState({
+         playerHand: playerHand.addCard(deck)
+       })
+       if (playerHand.busted) {
+         console.log("bust");
+         this.setState({
+         hitDisabled: true,
+         stayDisabled: true,
+         restartDisabled: false,
+         playerDone: true,
+         dealerDone: true,
+         message: "Player busted!"
+       })
+       }
+       break;
+     case "dealer":
+        console.log("dealer hit")
+        this.setState({
+          dealerHand: dealerHand.addCard(deck)
+        })
+        if (dealerHand.busted) {
+          this.setState({
+            dealerDone: true,
+            message: "Dealer Busted! Player Wins!"
+          })
+        }
+        else if (dealerHand.total >= 17 && dealerHand.total <= 21) {
+          this.setState({
+            dealerDone: true,
+            restartDisabled: false,
+          })
+        }
+        break;
+     case "aiPlayer":
+        break;
+     case "aiDealer":
+        break;
+      }
+  }
+
+  startGame() {
+    this.setState({
+      playerHand: [],
+      dealerHand: [],
+      playerDone: false,
+      dealerDone: false,
+      hitDisabled: false,
+      stayDisabled: false,
+      restartDisabled: true,
+      message: "hit or stay",
+    })
+    buildDeck()
+    dealCards()
+    if (this.checkForBlackjack(dealerHand, playerHand) == true) {
+      this.setState({
+        hitDisabled: true,
+        stayDisabled: true,
+        restartDisabled: false
+      })
+    }
+    else {
+      //get player moves until stay or bust
+      //get dealer moves
+      //let the ai play
+      //let the ai's dealer play
+      //add scores
+      //clear on restart
+    }
+
+    console.log('start')
+  }
+
+  stay() {
+    this.setState({
+      hitDisabled: true,
+      stayDisabled: true,
+      playerDone: true,
+    })
+    this.getDealerMoves()
   }
 
   render() {
     return (
       <Box style={gameStyle}>
         <Box style={playerSideStyle}>
+          <Box>
+            {this.state.message}
+          </Box>
           <Box style={dealerStyle}>
             Dealer
-            {getCards(dealerHand, "dealer")}
+            {this.getCards(dealerHand, "dealer")}
           </Box>
         <Box style={playerStyle}>
           Player
-          {getCards(playerHand, "player")}
+          {this.getCards(playerHand, "player")}
           <div style={gameButtons}>
-            <button onClick={() => this.hit()} style={gameButton}>HIT</button>
-            <button onClick={stay} style={gameButton}>STAY</button>
-            <button onClick={restart} style={gameButton}>PLAY AGAIN</button>
+            <button disabled={this.state.hitDisabled} onClick={() => this.hit("player")} style={gameButton}>HIT</button>
+            <button disabled={this.state.stayDisabled} onClick={() => this.stay()} style={gameButton}>STAY</button>
+            <button disabled={this.state.restartDisabled} onClick={() => this.startGame()} style={gameButton}>PLAY AGAIN</button>
           </div>
           </Box>
         </Box>
         <Box style={aiSideStyle}>
+          <Box>
+            {this.state.aiMessage}
+          </Box>
           <Box style={dealerStyle}>
             AI Dealer
-            {getCards(aiDealerHand, "dealer")}
+            {this.getCards(aiDealerHand, "dealer")}
           </Box>
           <Box style={aiStyle}>
             AI Player
-            {getCards(aiPlayerHand, "aiPlayer")}
+            {this.getCards(aiPlayerHand, "aiPlayer")}
           </Box>
         </Box>
       </Box>
     )
   }
-}
-
-function startGame() {
-  buildDeck()
-  dealCards()
-  //check for blackjack
-  //get player moves until stay or bust
-  //get dealer moves
-  //let the ai play
-  //let the ai's dealer play
-  //add scores
-  //clear on restart
-  console.log('start')
 }
 
 function buildDeck() {
@@ -163,37 +343,32 @@ function buildDeck() {
   console.log("building deck")
 }
 
-function dealCards() {
-  playerHand = [];
-  dealerHand = [];
-  aiPlayerHand = [];
-  aiDealerHand = [];
-  for (var i = 0; i < 2; i++) {
-    playerHand.push(deck.pop())
-    dealerHand.push(deck.pop())
-  }
-  aiPlayerHand = playerHand
-  aiDealerHand = dealerHand
-  console.log("dealing cards")
-}
 
-function getCards(hand, playerType) {
-  console.log(playerType, hand)
-  var cards = []
-  for (var i=0; i<hand.length; i++) {
-      var card = hand[i]
-      cards.push(getCard(card, i))
-      if (playerType === "dealer") {
-        i = hand.length
-      }
+
+function dealCards() {
+  playerHand.cards = [];
+  dealerHand.cards = [];
+  aiPlayerHand.cards = [];
+  aiDealerHand.cards = [];
+  for (var i = 0; i < 2; i++) {
+    playerHand.addCard(deck)
+    dealerHand.addCard(deck)
   }
-  return cards;
+  aiPlayerHand.cards = playerHand.cards.slice()
+  aiDealerHand.cards = dealerHand.cards.slice()
+  console.log("dealing cards")
 }
 
 function getCard(card, index) {
   return (
     <Image key={index.toString()} style={cardStyle} src={cardValues[card]}/>
   );
+}
+
+
+
+function restart() {
+
 }
 
 function shuffle(array) {
@@ -214,13 +389,11 @@ function shuffle(array) {
   return array;
 }
 
-function stay() {
 
-}
 
-function restart() {
 
-}
+
+
 
 // function getRandomIntInclusive(min, max) {
 //   min = Math.ceil(min);
