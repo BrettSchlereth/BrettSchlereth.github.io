7d5b6766e6ceef09e8105b5f3fbd957a5370bda6
Brett Schlereth <brschle@gmail.com>
2021-08-25 19:02:43 -0500

    Updates

---
+++ precache-manifest.27b5f0c0fe392ac1d1c9aef300d81a04.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de88bc91301ebc372d100a799a1fab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46b76e6553ac96cbcd26",
+    "url": "/static/css/main.1e17df9c.chunk.css"
+  },
+  {
+    "revision": "01b0a2a31ab14fe42086",
+    "url": "/static/js/2.564cc3b7.chunk.js"
+  },
+  {
+    "revision": "028f0e59b55ccca28936d6a582db55ef",
+    "url": "/static/js/2.564cc3b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b76e6553ac96cbcd26",
+    "url": "/static/js/main.34c47320.chunk.js"
+  },
+  {
+    "revision": "93ccebd0741681011358",
+    "url": "/static/js/runtime-main.bbbe84dd.js"
+  },
+  {
+    "revision": "567c4b33bf0a61b008c4660ca6dcc6c3",
+    "url": "/static/media/10ofSpades.567c4b33.png"
+  },
+  {
+    "revision": "1e17e2ed6281fb0aa8293c9fce110591",
+    "url": "/static/media/1920x620BunkerDown.1e17e2ed.png"
+  },
+  {
+    "revision": "d1039f102e0abe2b1706cf30f9225c15",
+    "url": "/static/media/4ofSpades.d1039f10.png"
+  },
+  {
+    "revision": "4a890ff47ecdcc46da1020fae9211652",
+    "url": "/static/media/5ofSpades.4a890ff4.png"
+  },
+  {
+    "revision": "2bb3954ab845903af27793c98d491c06",
+    "url": "/static/media/600x900BunkerDown.2bb3954a.png"
+  },
+  {
+    "revision": "465b083ddc77d8c6114b8270feb95c8f",
+    "url": "/static/media/6ofSpades.465b083d.png"
+  },
+  {
+    "revision": "397642b3ba98794c0c3ec7bcd9139a3b",
+    "url": "/static/media/7ofSpades.397642b3.png"
+  },
+  {
+    "revision": "a855651a7f05d2538429c34fc9191d25",
+    "url": "/static/media/8ofSpades.a855651a.png"
+  },
+  {
+    "revision": "1476e4c1c4319dee44473d614d7c2f25",
+    "url": "/static/media/9ofSpades.1476e4c1.png"
+  },
+  {
+    "revision": "6c27dff60dc8025c341ec2e70b0e352a",
+    "url": "/static/media/BlackjackImage.6c27dff6.png"
+  },
+  {
+    "revision": "87e620db0e2ee2fe1b99345c8d103b1c",
+    "url": "/static/media/D20Image.87e620db.png"
+  },
+  {
+    "revision": "859be2d87af56fe23ff850a06814fb07",
+    "url": "/static/media/ResumeImage.859be2d8.png"
+  },
+  {
+    "revision": "001171ec028332d94500b0622a2bac1a",
+    "url": "/static/media/aceOfSpades.001171ec.png"
+  }
+]);
