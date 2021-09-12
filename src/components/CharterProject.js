0738fac97eb07e84a57cfb68dd8aefbdcb75015b
brsgm5 <brsgm5@mail.umsl.edu>
2021-09-12 18:24:27 -0500

    Added some styling to the button and added some comments

--- src/components/CharterProject.js
+++ src/components/CharterProject.js
@@ -1,53 +1,96 @@
 import React from 'react';
 import initSqlJs from 'sql.js';
 
-
-//https://github.com/BrettSchlereth/CharterHW/blob/main/transactionSQL.sqlite?raw=true
+const buttonStyle = {
+  false: {
+    textAlign: 'center',
+    height: '10%',
+    width: '25%',
+    backgroundColor: "white",
+    color: "black",
+    fontSize: "1.5em",
+    borderRadius: '5px',
+    border: '1px solid black',
+    fontWeight: 'bold',
+  },
+  true: {
+    textAlign: 'center',
+    height: '10%',
+    width: '25%',
+    backgroundColor: "gray",
+    color: "black",
+    fontSize: "1.5em",
+    borderRadius: '5px',
+    border: '1px solid black',
+    fontWeight: 'bold',
+  }
+};
 
 const dataStyle = {
   whiteSpace: 'pre-wrap'
 }
 
-var database
-
 class CharterProject extends React.Component {
-
   constructor(props) {
     super(props);
+    //Loads the dataset at the starting of the page
     this.loadDataset()
     this.state={
       database: null,
-      SQLresponse: null,
+      points: null,
     }
-
   }
 
+  //Given one customer's name, this will return the customer's total points
+  //earned from all of their transactions
   calculatePoints(name) {
+    //Queries the SQLite file for the customer's transactions
     var amounts = this.getQuery("SELECT Amount FROM transactionData WHERE CustomerName='" + name + "'")
-    console.log("amounts: ", amounts)
-    var points = 0
-    for (var i = 0; i < amounts.length) {
-      var amount = amounts[i][0]
+    let points = 0
+    //Calculates the points based on the transaction amount
+    for (var i = 0; i < amounts.length; i++) {
+      var amount = Math.floor(amounts[i][0])
       if (amount > 100) {
-        points += 2 * (amounts)
+        points += 2 * (amount - 100) + 50
+      }
+      else if (amount > 50) {
+        points += (amount - 50)
       }
     }
+    //Returns the customer's total points
+    return points
   }
 
+  //Fetches a list of every customer and calculates each of their point totals
   getPoints() {
+    //Queries the SQLite file for a list of every customer name
     var namesArr = this.getQuery("SELECT DISTINCT CustomerName FROM transactionData")
+    //Creates a dictionary that will hold each customer's name and their total points
     var names = {}
+    //Assigns the total points to each name
     for (var i = 0; i < namesArr.length; i++) {
-      console.log("name: ", namesArr[i][0])
-      names[namesArr[i][0]] = calculatePoints(namesArr[i][0]);
-      //this.calculatePoints(names[i][0])
+      names[namesArr[i][0]] = this.calculatePoints(namesArr[i][0]);
     }
+    //Sets the points state to equal a string with all of the names and points
+    this.setState({
+      points: this.printData(names)
+    })
   }
 
+  //Returns the values of a given SQL query
   getQuery(query) {
     return this.state.database.exec(query)[0]["values"]
   }
 
+  //Disables the button after pressing the button
+  isDisabled() {
+    if (this.state.points != null) {
+      return true
+    }
+    return false
+  }
+
+  //Fetches the database from the public files and stores it in the db state value
   async loadDataset () {
     const sqlPromise = initSqlJs({locateFile: file => `sql-wasm.wasm`});
     const dataPromise = fetch("transactionSQL.sqlite").then(res => res.arrayBuffer());
@@ -58,62 +101,31 @@ class CharterProject extends React.Component {
           database: db
         })
     }
+    //Throws an error if the database fails to load
     catch (err) {
       console.log(err);
       console.log("Failed to load database")
     }
-    console.log("db: ", this.state.database)
   }
 
-
-
-  //QUERIES
-  //"SELECT DISTINCT CustomerName FROM transactionData"
-
-  showSomething() {
-    //console.log("response: ", this.state.database.exec("SELECT * FROM transactionData WHERE CustomerName='John'")[0]["values"])
-    this.setState({
-      SQLresponse: this.getQuery("SELECT Amount FROM transactionData WHERE CustomerName='John'")
-    })
-    this.getPoints()
-  }
-
+  //Formats the given dictionary into a string format to be printed on screen
   printData(data) {
     if (data != null) {
       var output = ""
-      for (var i = 0; i < data.length; i++) {
-        output += String(data[i]) + "\n"
+      for (const [key, value] of Object.entries(data)) {
+        output += String(key) + ": " + String(value) + "\n"
       }
       return output
     }
-    else return "goodbye"
   }
-
-
-  // var db = new SQL.Database(new Uint8Array(buf));
-  // console.log("db: ", db)
-  // return db
-
-
-    // const db = new SQL.Database(new Uint8Array(buf));
-
-    // this.setState({
-    //   database: new SQL.Database(new Uint8Array(buf))
-    // })
-    // res = db.exec("SELECT * FROM transactionData WHERE TransactionId=4")
-
-    // this.setState({
-    //   SQLresponse: db.exec("SELECT * FROM transactionData WHERE TransactionId=4")[0]["values"][0][2]
-    // })
-//{this.state.database.exec("SELECT * FROM transactionData WHERE TransactionID=4")[0]["values"][0][2]}
 
   render() {
     return(
       <div>
-      <p style={dataStyle}>
-        {this.printData(this.state.SQLresponse)}
-      </p>
-        <button onClick={() => this.showSomething()}> hit me </button>
+        <p style={dataStyle}>
+          {this.state.points}
+        </p>
+        <button style={buttonStyle[this.isDisabled()]} disabled={this.isDisabled()} onClick={() => this.getPoints()}> Get Points </button>
       </div>)
   }
 }
